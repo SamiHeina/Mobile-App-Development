7d388acd19234d2dccecd510c35135f1bdbf7ca5
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-10-25 14:01:48 +0300

    Nyt pitäis toimii

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,51 +1,22 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-=======
->>>>>>> Stashed changes
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-<<<<<<< Updated upstream
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-=======
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
->>>>>>> Stashed changes
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-<<<<<<< Updated upstream
   },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },  {
+  },  {
     path: 'settings',
     loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
-=======
-  },  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
-  },
->>>>>>> Stashed changes
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
->>>>>>> Stashed changes
   },
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
