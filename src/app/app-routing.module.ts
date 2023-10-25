885991c4fc8ee5ddab924733ded32d5a2b4bd59d
ToniGrondahl <toni.grondahl@outlook.com>
2023-10-25 12:22:38 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+=======
+>>>>>>> Stashed changes
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+<<<<<<< Updated upstream
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+=======
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+>>>>>>> Stashed changes
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+<<<<<<< Updated upstream
   },
   {
     path: 'home',
@@ -17,6 +30,22 @@ const routes: Routes = [
   },  {
     path: 'settings',
     loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+=======
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+>>>>>>> Stashed changes
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+>>>>>>> Stashed changes
   },
 
 
