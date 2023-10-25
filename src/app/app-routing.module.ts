54adf2d5bac7172887ec3b3dcd886667f7379c09
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-10-25 13:57:44 +0300

    sori äijjät, sekoilin tän kans

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },  {
+  },  {
     path: 'settings',
     loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
   },
