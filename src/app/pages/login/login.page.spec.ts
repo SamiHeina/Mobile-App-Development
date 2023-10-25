7e8c4c453cceb16fb4f0071736492dbd8d86daec
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-10-25 13:27:42 +0300

    Pitäis workkiii

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -15,3 +15,9 @@ describe('LoginPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+//Note this if theres problem with async function in the future
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
+
