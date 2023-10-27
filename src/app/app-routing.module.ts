26ea5047e1c24b2c30d9d36d128fc11d28938d7e
ToniGrondahl <toni.grondahl@outlook.com>
2023-10-27 13:37:02 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,10 +15,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
-  {
-    path: 'ravintolat',
-    loadChildren: () => import('./ravintolat/ravintolat.module').then(m => m.RavintolatPageModule)
-  },
 
   {
     path: 'settings',
